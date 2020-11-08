abeca9ee47be6be322e26949f0b9f4be6574eaef
attrighosal <attrighosal@gmail.com>
2020-11-08 13:02:54 +0530

    Added the guitar class

---
+++ Guitar_Shop/Guitar.java
@@ -0,0 +1,46 @@
+package Guitar_Shop; 
+
+import java.lang.String;
+
+class Guitar{ 
+    private String serialNumber, builder, model, type, backWood, topWood; 
+    private double price; 
+
+    Guitar(String serialNumber, double price, String builder, String model, String type, String backWood, String topWood){
+        this.serialNumber = serialNumber; 
+        this.price = price;   
+        this.builder = builder; 
+        this.model = model; 
+        this.type = type; 
+        this.backWood = backWood; 
+        this.topWood = topWood; 
+    } 
+
+    public String getSerialNumber(){ 
+        return this.serialNumber; 
+    } 
+
+    public double getPrice(){ 
+        return this.price; 
+    } 
+
+    public String getBuilder(){ 
+        return this.builder; 
+    } 
+
+    public String getModel(){ 
+        return this.model; 
+    } 
+
+    public String getType(){ 
+        return this.type; 
+    }  
+
+    public String getTopWood(){ 
+        return this.topWood; 
+    }  
+
+    public String getBackWood(){ 
+        return this.backWood;
+    } 
+}
